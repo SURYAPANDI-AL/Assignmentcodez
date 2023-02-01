0f651772a1961a4c1437195870456ca1be6bde4a
SURYAPANDI_AL <121593379+SURYAPANDI-AL@users.noreply.github.com>
2023-02-01 22:55:54 +0530

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,21 @@
+//bubble sort.
+/*
+*the last element get sorted accordingly while 
+*swaping the adjacent elements in the array.
+*/
+class BubbleSort
+{
+    public void bubbleSort(int [] nums)
+    {
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=0;j<nums.length-i-1;j++)
+            {
+                if(nums[j]>nums[j+1])
+                {
+                    nums[j]=nums[j+1]+nums[j]-(nums[j+1]=nums[j]);
+                }
+            }
+        }
+    }
+}
