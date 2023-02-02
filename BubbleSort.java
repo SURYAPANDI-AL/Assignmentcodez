4f2f9a908c6138a1118c3b70c91803abf15e9b00
SURYAPANDI_AL <121593379+SURYAPANDI-AL@users.noreply.github.com>
2023-02-02 10:22:41 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -7,7 +7,7 @@ class BubbleSort
 {
     public void bubbleSort(int [] nums)
     {
-        for(int i=0;i<nums.length;i++)
+        for(int i=0;i<nums.length-1;i++)
         {
             for(int j=0;j<nums.length-i-1;j++)
             {
